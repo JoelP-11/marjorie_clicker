b22d73449e98cdb82b509782e6755798aed13a69
JoelP-11 <Jp421513@gmail.com>
2023-06-12 23:03:39 -0700

    done

--- script.js
+++ script.js
@@ -3,13 +3,29 @@ let score = 0;
 // Get the score element and the image element
 const scoreElement = document.getElementById('score');
 const imageElement = document.getElementById('image');
+const imageContainer = document.getElementById('image-container');
 
-// Increment the score when the image is clicked
-imageElement.addEventListener('click', function() {
+function createTinyImage() {
+  const tinyImage = document.createElement('img');
+  tinyImage.src = imageElement.src;
+  tinyImage.classList.add('tiny-image');
+  
+  const leftPosition = Math.random() * (imageContainer.offsetWidth - 50);
+  tinyImage.style.left = `${leftPosition}px`;
+
+  imageContainer.appendChild(tinyImage);
+  setTimeout(function() {
+    imageContainer.removeChild(tinyImage);
+  }, 1000);
+}
+
+// Function to update the score and check for bounce effect
+function updateScore() {
   score++;
-  scoreElement.textContent = `Score: ${score}`;
-  imageElement.classList.add('bounce');
-setTimeout(function() {
-    imageElement.classList.remove('bounce');
-}, 500);
-});
+  scoreElement.textContent = `Marjories killed: ${score}`;
+
+  createTinyImage();
+}
+
+// Increment the score and check for bounce effect when the image is clicked
+imageElement.addEventListener('click', updateScore);
